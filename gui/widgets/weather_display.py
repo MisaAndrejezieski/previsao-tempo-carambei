7fce4c22d15889811ae111056fba65a66ed0f810
MisaAndrejezieski <165926051+MisaAndrejezieski@users.noreply.github.com>
2026-08-15 16:52:15 -0300

    Update weather_display.py

--- gui/widgets/weather_display.py
+++
@@ -1,8 +0,0 @@
-import tkinter as tk
-from datetime import datetime
-from tkinter import scrolledtext
-from typing import Optional
-
-from core.models import PrevisaoCompleta
-from gui.styles import NeonTheme
-from utils.logger import log
